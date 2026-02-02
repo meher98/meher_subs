fc0c371bac61ece87f33161a13f3c8af8fcd5d21
Meher Ben Mahmoud <meher.benmahmoud@ensi-uma.tn>
2026-02-02 19:28:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { addonBuilder, serveHTTP } = require("stremio-addon-sdk")
+
+const manifest = {
+    id: "org.me.meher.subs",
+    version: "1.0.0",
+    name: "My Subtitles",
+    description: "Personal subtitles addon",
+    resources: ["subtitles"],
+    types: ["series", "movie", "anime"],
+    catalogs: [],
+    idPrefixes: ["tt"]
+}
+
+const builder = new addonBuilder(manifest)
+
+function pad(n) {
+    return String(n).padStart(2, "0")
+}
+
+builder.defineSubtitlesHandler(async ({ id, extra }) => {
+    const { season, episode } = extra || {}
+    if (!season || !episode) return { subtitles: [] }
+
+    const s = pad(season)
+    const e = pad(episode)
+
+    return {
+        subtitles: [
+            {
+                id: `${id}-s${s}e${e}-fr`,
+                lang: "fr",
+                url: `https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/${id}/s${s}e${e}.fr.srt`
+            }
+        ]
+    }
+})
+
+const PORT = process.env.PORT || 7000
+
+serveHTTP(builder.getInterface(), { port: PORT })
